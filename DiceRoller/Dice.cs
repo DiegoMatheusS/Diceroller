ed81d2e5c92e9896b3a7bef635f249d46a110428
Diego Matheus <dms.diego.2019@gmail.com>
2024-04-15 22:14:28 -0300

    feat: adicionado metodos

---
+++ DiceRoller/Dice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    class Dice
+    {
+        private int Lados { get; set; }
+
+        public Dice() { }
+
+        public Dice(int lados)
+        {
+            Lados = lados;
+        }
+
+        //Atributos
+
+
+        //Métodos
+        public int Rolar()
+        {
+            //Retorna o valor da rolagem do dado.
+            return new Random().Next(1, Lados+1);
+        }
+    }
+}
---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        int valor = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            DiceTypePicker.SelectedIndex = 0;
+        }
+
+        private void RollBtn_Clicked(object sender, EventArgs e)
+        {
+
+            //Pegar o valor que foi selecionado pelo usuario
+            valor = Convert.ToInt32(DiceTypePicker.SelectedItem);
+            //jogar esse valor em uma variavel
+            //Sortear um numero de um dado utilizando o Random
+            //Colocar o valor sorteado em uma variavel
+
+
+            int valorSorteado = 0;
+            Dice dice = new Dice(valor);
+
+            valorSorteado = dice.Rolar();
+            //Exibir o vlaor para o usuario na resultLabel
+            resultLabel.Text = valorSorteado.ToString();
+
+        }
+    }
+
+}
